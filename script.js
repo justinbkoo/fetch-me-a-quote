a1548c868d3c20f01443d38791ac573d37e1a2fe
Justin Koo <bjustinkoo@gmail.com>
2017-05-29 16:28:23 -0500

    Added twitter button for users tweet their quote.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var link = "";
+
 function randomColorValue() {
   return Math.floor((Math.random() * 255));
 }
@@ -19,6 +21,7 @@ function loadNewQuote() {
 		success: function(data) {
 			var quote = data[0].content;
 			var author = data[0].title;
+      link = data[0].link;
       
       $('#quote').addClass('animated fadeIn');
 	    $('#quote').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
@@ -40,6 +43,19 @@ function loadNewQuote() {
 	});
 }
 
+function openTweetWindow() {
+	var url="https://twitter.com/intent/tweet";
+    var quote = $("#quote").text();
+	//quote = quote.replace(\<(?:.|\n)*?>/gm, '');
+    var author = $("#author").text();
+    if(quote.length + author.length + 1 > 140) {
+      alert("The tweet will be over 140 characters. A link will be provided to shorten it.");
+      window.open(url+"?text="+link, "", "width=500,height=300");
+    } else {
+      window.open(url+"?text="+quote+" "+author, "", "width=500,height=300");
+    }
+}
+
 $(document).ready(function() {
 	loadNewQuote();
 	loadNewBackgroundAndTextColor();
@@ -50,5 +66,5 @@ $(document).ready(function() {
 	$("#infoButton").on('click', function() {
 		$('#quote').html("Created by <a href=\"https://justinbkoo.github.io/\">Justin Koo</a> using <a href=\"https://quotesondesign.com/api-v4-0/\"><em>Quotes on Design</em></a> API");
 		$('#author').html("Copyright &copy 2017 Justin Koo");
-	});	
+	});
 });
